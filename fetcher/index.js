6fa663e12c062b824e3f4637a2ec02e36427b27c
Bæring Gunnar Steinþórsson <baering90@gmail.com>
2016-10-05 20:58:06 +0000

    Changing fetcher temporarily

--- fetcher/index.js
+++ fetcher/index.js
@@ -2,15 +2,29 @@ import fetchVotes from './votes'
 import fetchSpeeches from './speeches'
 import { writeToFile } from '../utility/file'
 
-// fetchVotes()
-//   .then(data => {
-//     console.log('Successfully fetched votes')
-//     writeToFile(data, 'data/all-votes.json')
-//   }).catch(error => {
-//     console.log(`There was an error: ${error}`)
-//   })
+fetchVotes()
+  .then(data => {
+    console.log('Successfully fetched votes')
+    writeToFile(data, 'data/all-votes.json')
+  }).catch(error => {
+    console.log(`There was an error: ${error}`)
+  })
 
 // fetchSpeeches().then(speeches => {
 //   console.log('Successfully fetched all speeches')
 //   writeToFile(speeches, 'data/all-speeches-.json')
 //   }).catch((error) => console.log(`There was an error: ${error}`))
+
+// import fetchMps from './mps'
+//
+// fetchMps()
+//   .then(mps => {
+//     writeToFile(mps, 'data/mps.json', true)
+//   })
+
+// import fetchSubjects from './subjects'
+//
+// fetchSubjects(145)
+//   .then(subjects => {
+//     writeToFile(subjects, 'data/subjects.json', true)
+//   }).catch(error => console.log(`Error: ${error}`))
