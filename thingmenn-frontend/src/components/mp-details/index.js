620545eceb1d936e963b1410d8e226bb63838c5c
Bæring Gunnar Steinþórsson <baering90@gmail.com>
2016-10-15 01:16:33 +0000

    Frontend: display mp vote summary

--- thingmenn-frontend/src/components/mp-details/index.js
+++ thingmenn-frontend/src/components/mp-details/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import classnames from 'classnames';
 import 'whatwg-fetch'
 
 import './styles.css';
+
+function fetchJson(url) {
+  console.log(`Fetching json: ${url}`)
+  return fetch(url).then(response => response.json())
+}
 
 export default class Mps extends React.Component {
   constructor(props) {
@@ -10,33 +14,47 @@ export default class Mps extends React.Component {
 
     this.state = {
       mp: {},
+      voteSummary: {
+        voteSummary: {},
+        votePercentages: {},
+      },
     }
   }
 
   componentDidMount() {
     const { mpId } = this.props.params
 
-    fetch(`http://localhost:8080/api/mps/${mpId}`)
-      .then(response => {
-        return response.json()
-      })
-      .then(mp => {
-        this.setState({
-          mp,
-        })
-      })
-      .catch(error => {
-        console.log(`Error: ${error}`)
-      })
+    const mpUrl = `http://localhost:8080/api/mps/${mpId}`
+    fetchJson(mpUrl)
+      .then(mp => this.setState({ mp }))
+      .catch(error => console.log(error))
+
+    const mpVoteUrl = `http://localhost:8080/api/summary/votes/mp/${mpId}`
+    fetchJson(mpVoteUrl)
+      .then(voteSummary => this.setState({ voteSummary }))
+      .catch(error => console.log(error))
+
+    // fetch(`http://localhost:8080/api/mps/${mpId}`)
+    //   .then(response => {
+    //     return response.json()
+    //   })
+    //   .then(mp => {
+    //     this.setState({
+    //       mp,
+    //     })
+    //   })
+    //   .catch(error => {
+    //     console.log(`Error: ${error}`)
+    //   })
   }
 
   render() {
-    const { className } = this.props;
-    const { mp } = this.state
+    // const { className } = this.props;
+    const { mp, voteSummary } = this.state
 
     return (
       <div className='mp-details'>
-        <div className='mp-details__introduction'>
+        <div className='mp-details__introduction mp-details__section'>
           <div
             className='mp-details__introduction-image'
             style={{
@@ -44,9 +62,18 @@ export default class Mps extends React.Component {
             }}
           ></div>
           <div className='mp-details__introduction-text'>
-            <h1 className='mp-details__introduction-name'>{mp.name}</h1>
-            <h2 className='mp-details__introduction-party'>{mp.party}</h2>
+            <h1 className='mp-details__introduction-name heading'>{mp.name}</h1>
+            <h2 className='mp-details__introduction-party text'>{mp.party}</h2>
           </div>
+        </div>
+        <div className='mp-details__votes mp-details__section'>
+          <h2 className='heading'>Yfirlit atkvæða</h2>
+          <ul>
+            <li className='text'>{voteSummary.voteSummary.numberOfVotes} atkvæði</li>
+            <li className='text'>{voteSummary.votePercentages.standsTaken}% afstaða tekin</li>
+            <li className='text'>{voteSummary.votePercentages.idle}% hlutleysi</li>
+            <li className='text'>{voteSummary.votePercentages.away}% fjarverandi</li>
+          </ul>
         </div>
       </div>
     );
