0b85af9356d91263f5fa2f7fce738bdaffa9ce79
Bæring Gunnar Steinþórsson <baering90@gmail.com>
2016-10-16 01:51:28 +0000

    Backend: rename mps directory

--- thingmenn-api/api/router.py
+++ thingmenn-api/api/router.py
@@ -1,4 +1,4 @@
-from api.mp import get_mps, get_mp_by_id
+from api.mps import get_mps, get_mp_by_id
 from api.summary import get_mp_vote_summary, get_mp_subject_summary, get_mp_nouns
 
 from flask import blueprints
@@ -7,12 +7,9 @@ from flask import make_response
 
 api = blueprints.Blueprint('thingmenn-api', __name__)
 
-prefix = '/api'
+api.add_url_rule('/api/mps', 'list_mps', get_mps)
+api.add_url_rule('/api/mps/<int:mp_id>', 'get_mp', get_mp_by_id)
 
-api.add_url_rule(prefix + '/mps', 'list_mps', get_mps)
-api.add_url_rule(prefix + '/mps/<int:mp_id>', 'get_mp', get_mp_by_id)
-
-summary_prefix = prefix + '/summary'
-api.add_url_rule(summary_prefix + '/votes/mp/<string:mp_id>', 'get_mp_vote_summary', get_mp_vote_summary)
-api.add_url_rule(summary_prefix + '/subjects/mp/<string:mp_id>', 'get_mp_subject_summary', get_mp_subject_summary)
-api.add_url_rule(summary_prefix + '/nouns/mp/<string:mp_id>', 'get_mp_nouns', get_mp_nouns)
+api.add_url_rule('/api/summary/votes/mp/<string:mp_id>', 'get_mp_vote_summary', get_mp_vote_summary)
+api.add_url_rule('/api/summary/subjects/mp/<string:mp_id>', 'get_mp_subject_summary', get_mp_subject_summary)
+api.add_url_rule('/api/summary/nouns/mp/<string:mp_id>', 'get_mp_nouns', get_mp_nouns)
