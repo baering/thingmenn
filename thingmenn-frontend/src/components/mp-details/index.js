31af6815b1d37753fa8dc1576f9c7990d6b3fa05
Aegir Giraldo Thorsteinsson <aegir@thorsteinsson.is>
2016-10-18 23:36:49 +0000

    Header for details page

--- thingmenn-frontend/src/components/mp-details/index.js
+++ thingmenn-frontend/src/components/mp-details/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'whatwg-fetch'
 
-import './styles.css';
+import MpHeader from '../../widgets/mp-header'
 
 function fetchJson(url) {
   console.log(`Fetching json: ${url}`)
@@ -56,28 +56,7 @@ export default class Mps extends React.Component {
 
     return (
       <div className='mp-details'>
-        <div className='mp-details__introduction mp-details__section'>
-          <div
-            className='mp-details__introduction-image'
-            style={{
-              backgroundImage: `url(${mp.imagePath})`
-            }}
-          ></div>
-          <div className='mp-details__introduction-text'>
-            <h1 className='mp-details__introduction-name heading'>{mp.name}</h1>
-            <h2 className='mp-details__introduction-party text'>{mp.party}</h2>
-          </div>
-        </div>
-
-        <div className='mp-details__votes mp-details__section'>
-          <h2 className='heading'>Yfirlit atkvæða</h2>
-          <ul>
-            <li className='text'>{voteSummary.voteSummary.numberOfVotes} atkvæði</li>
-            <li className='text'>{voteSummary.votePercentages.standsTaken}% afstaða tekin</li>
-            <li className='text'>{voteSummary.votePercentages.idle}% hlutleysi</li>
-            <li className='text'>{voteSummary.votePercentages.away}% fjarverandi</li>
-          </ul>
-        </div>
+        <MpHeader voteSummary={voteSummary} {...mp} />
 
         <div className='mp-details__nouns mp-details__section'>
           <h3 className='heading'>Mest talað um</h3>
---
+++ thingmenn-frontend/src/widgets/mp-header/index.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import './styles.css'
+
+const MpHeader = ({
+  id,
+  name,
+  party,
+  imagePath,
+  voteSummary,
+}) => {
+  return (
+    <div className="MpHeader">
+      <div className="MpHeader-details">
+        <div
+          className="MpHeader-image"
+          style={{
+            backgroundImage: `url(${imagePath})`
+          }}
+        />
+        <div className="MpHeader-bio">
+          <h1 className="MpHeader-bioName">{name} <span className="Main-subheader">{party}</span></h1>
+          <p className="MpHeader-bioText"></p>
+        </div>
+        <div className="MpHeader-stats">
+          <div className="MpHeader-statsItem">
+            <p className="MpHeader-statsText">{voteSummary.voteSummary.numberOfVotes}</p>
+            <h1 className="MpHeader-statsHeading">Atkvæði</h1>
+          </div>
+          <div className="MpHeader-statsItem">
+            <p className="MpHeader-statsText">{voteSummary.votePercentages.standsTaken}%</p>
+            <h1 className="MpHeader-statsHeading">Afstaða</h1>
+          </div>
+          <div className="MpHeader-statsItem">
+            <p className="MpHeader-statsText">{100 - voteSummary.votePercentages.away}%</p>
+            <h1 className="MpHeader-statsHeading">Mæting</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+MpHeader.propTypes = {
+  id: React.PropTypes.string,
+  name: React.PropTypes.string,
+  profilePicture: React.PropTypes.string,
+  party: React.PropTypes.string,
+}
+
+export default MpHeader
