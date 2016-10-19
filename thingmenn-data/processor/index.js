d49425a828e695136f4645a42d42a0c613bc891e
Bæring Gunnar Steinþórsson <baering90@gmail.com>
2016-10-19 00:09:08 +0000

    Data: update fetcher and processor index files

--- thingmenn-data/processor/index.js
+++ thingmenn-data/processor/index.js
@@ -1,9 +1,17 @@
-import createFriendLookup from './votes'
-import createMpNounLookup from './speeches'
-import createMpPositionLookup from './position'
+// import createFriendLookup from './votes'
 import { writeToFile } from '../utility/file'
 
 // const friendLookup = createFriendLookup()
 // writeToFile(friendLookup, 'data/friend-lookup.json', true)
+
+// import createMpNounLookup from './speeches'
 // createMpNounLookup()
-createMpPositionLookup()
+
+// import createMpPositionLookup from './position'
+// createMpPositionLookup()
+
+import createMpSummary from './mp-summary'
+createMpSummary()
+
+// import createPartySummaries from './parties'
+// createPartySummaries()
