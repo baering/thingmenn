b86768c66397932437b5e7e018a97db195f5eb32
David Bachmann <davidbachmann@Davids-MacBook-Air.local>
2016-10-19 17:24:33 +0000

    Responsify

--- thingmenn-frontend/src/widgets/mp-header/index.js
+++ thingmenn-frontend/src/widgets/mp-header/index.js
@@ -17,24 +17,24 @@ const MpHeader = ({
           style={{
             backgroundImage: `url(${imagePath})`
           }}
-        />
+        ></div>
         <div className="MpHeader-bio">
           <h1 className="MpHeader-bioName">{name} <span className="Main-subheader">{party}</span></h1>
           <p className="MpHeader-bioText">{description}</p>
         </div>
-        <div className="MpHeader-stats">
-          <div className="MpHeader-statsItem">
-            <p className="MpHeader-statsText">{voteSummary.voteSummary.numberOfVotes}</p>
-            <h1 className="MpHeader-statsHeading">Atkvæði</h1>
-          </div>
-          <div className="MpHeader-statsItem">
-            <p className="MpHeader-statsText">{voteSummary.votePercentages.standsTaken}%</p>
-            <h1 className="MpHeader-statsHeading">Afstaða</h1>
-          </div>
-          <div className="MpHeader-statsItem">
-            <p className="MpHeader-statsText">{voteSummary.votePercentages.away}%</p>
-            <h1 className="MpHeader-statsHeading">Fjarverandi</h1>
-          </div>
+      </div>
+      <div className="MpHeader-stats">
+        <div className="MpHeader-statsItem">
+          <p className="MpHeader-statsText">{voteSummary.voteSummary.numberOfVotes}</p>
+          <h1 className="MpHeader-statsHeading">Atkvæði</h1>
+        </div>
+        <div className="MpHeader-statsItem">
+          <p className="MpHeader-statsText">{voteSummary.votePercentages.standsTaken}%</p>
+          <h1 className="MpHeader-statsHeading">Afstaða</h1>
+        </div>
+        <div className="MpHeader-statsItem">
+          <p className="MpHeader-statsText">{voteSummary.votePercentages.away}%</p>
+          <h1 className="MpHeader-statsHeading">Fjarverandi</h1>
         </div>
       </div>
     </div>
