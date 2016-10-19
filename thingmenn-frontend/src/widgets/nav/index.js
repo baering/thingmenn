5b0788a5015244a217b362f1ddc689866af9e05f
David Bachmann <dabbi@dabbi.is>
2016-10-19 18:14:40 +0000

    Temporary mobile menu. Responsive and typography fixes.

--- thingmenn-frontend/src/widgets/nav/index.js
+++ thingmenn-frontend/src/widgets/nav/index.js
@@ -6,7 +6,9 @@ class Nav extends React.Component {
     return (
       <nav className="Nav">
         <div className="Nav-top">
-          <h1>Þingmenn.is</h1>
+          <h1 className="Nav-title">Þingmenn.is</h1>
+          <h1 className="Nav-title--shorter">Þingmenn</h1>
+          <h1 className="Nav-title--shortest">ÞM</h1>
         </div>
         <a href="/" className="Nav-item is-active"><span className="typcn typcn-user-outline"></span> Þingmenn</a>
         {/*<a href="#" className="Nav-item"><span className="typcn typcn-group-outline"></span> Flokkar</a>*/}
