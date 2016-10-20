9dcd1d3c0f9485fc3dd7d094dabd6e60392f312b
herrhelms <dev@straw.io>
2016-10-20 22:12:32 +0000

    :bar_chart:

--- thingmenn-frontend/src/widgets/bar-chart/index.js
+++ thingmenn-frontend/src/widgets/bar-chart/index.js
@@ -6,25 +6,36 @@ const BarChart = ({
 }) => {
   const { subject, voteSplit } = subjectSummary
 
-  let standsTaken = 0
-  let idle = 0
-  let away = 0
+  let standsTaken = 0;
+  let standsTakenOccurance;
+  let idle = 0;
+  let idleOccurance;
+  let away = 0;
+  let awayOccurance;
 
   if (voteSplit.standsTaken) {
     standsTaken = voteSplit.standsTaken.percentage
+    standsTakenOccurance = voteSplit.standsTaken.occurance
   }
 
   if (voteSplit.idle) {
     idle = voteSplit.idle.percentage
+    idleOccurance = voteSplit.idle.occurance
   }
 
   if (voteSplit.away) {
     away = voteSplit.away.percentage
+    awayOccurance = voteSplit.away.occurance
   }
 
   return (
-    <div>
-      <strong>{subject}</strong> {standsTaken}% / {idle}% / {away}% = {standsTaken + idle + away}%
+    <div className="Barchart">
+      <div className="Barchart-topic"><strong>{subject}</strong></div>
+      <div className="Barchart-bar">
+        <div className="Barchart-segment Barchart--standsTaken" style={{width: standsTaken + '%'}}>{standsTakenOccurance}</div>
+        <div className="Barchart-segment Barchart--idle" style={{width: idle + '%'}}>{idleOccurance}</div>
+        <div className="Barchart-segment Barchart--away" style={{width: away + '%'}}>{awayOccurance}</div>
+      </div>
     </div>
   )
 }
