36e9154ee185eefa1b78d141f97658b58c0e6d39
David Bachmann <dabbi@dabbi.is>
2016-10-21 00:44:10 +0000

    Added footer

--- thingmenn-frontend/src/index.js
+++ thingmenn-frontend/src/index.js
@@ -4,19 +4,23 @@ import ReactDOM from 'react-dom'
 import Routes from './routes'
 
 import Nav from './widgets/nav'
+import Footer from './widgets/footer'
 
 import './reset.css'
 import './index.css'
 import './typography.css'
 
 ReactDOM.render(
-  <div className="contain">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500|Lora" rel="stylesheet" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/typicons/2.0.8/typicons.min.css" rel="stylesheet" />
-    <Nav />
-    <main className="fill">
-      <Routes />
-    </main>
+  <div className="abc">
+    <div className="contain">
+      <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500|Lora" rel="stylesheet" />
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/typicons/2.0.8/typicons.min.css" rel="stylesheet" />
+      <Nav />
+      <main className="fill">
+        <Routes />
+      </main>
+    </div>
+    <Footer />
   </div>,
   document.getElementById('root')
 );
---
+++ thingmenn-frontend/src/widgets/footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './styles.css'
+
+const Footer = () => {
+  return (
+    <div className='Footer'>
+      Gögn birt með fyrirvara um villur í úrvinnslu. Athugasemdir þegnar á athugasemdir@thingmenn.is
+    </div>
+  )
+}
+
+Footer.propTypes = {
+
+}
+
+export default Footer
