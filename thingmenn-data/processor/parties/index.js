dfd81df3d4b31580cb8f622db7d57f22b8abca35
Bæring Gunnar Steinþórsson <baering90@gmail.com>
2016-10-21 18:27:53 +0000

    Data: clean up parties processor

--- thingmenn-data/processor/parties/index.js
+++ thingmenn-data/processor/parties/index.js
@@ -1,9 +1,6 @@
 import { loadFile, writeToFile } from '../../utility/file'
 
 const mps = loadFile('data/mps.json')
-const mpNouns = loadFile('data/export/mp-noun-lookup.json')
-const mpPositions = loadFile('data/export/mp-positions.json')
-const mpSimilarVotes = loadFile('data/export/mp-similar-votes.json')
 const mpVoteSummaries = loadFile('data/export/mp-vote-summaries.json')
 
 const mpIdToParty = {}
@@ -58,84 +55,6 @@ function calculateVotePercentages(partyNames, voteSummary) {
   })
 }
 
-function updateNounSummary(nounSummary, topNounsForMp, mp) {
-  if (!nounSummary[mp.party]) {
-    nounSummary[mp.party] = {}
-  }
-
-  topNounsForMp.forEach(noun => {
-    if (!nounSummary[mp.party][noun.noun]) {
-      nounSummary[mp.party][noun.noun] = 0
-    }
-
-    nounSummary[mp.party][noun.noun] += noun.occurance
-  })
-}
-
-function getTopNouns(nounSummary, partyNames) {
-  const topPartyNouns = {}
-  partyNames.forEach(partyName => {
-    const partyNounSummary = nounSummary[partyName]
-
-    const nouns = Object.keys(partyNounSummary)
-    topPartyNouns[partyName] = nouns.sort((a, b) => {
-      const occuranceA = nounSummary[partyName][a]
-      const occuranceB = nounSummary[partyName][b]
-
-      return occuranceB - occuranceA
-    }).slice(0, 20).map(noun => {
-      return {
-        noun,
-        occurance: nounSummary[partyName][noun]
-      }
-    })
-  })
-
-  return topPartyNouns
-}
-
-// TODO: Need to revisit this, not enough to know how many votes
-//       are similar, need to know which votes to not recount
-
-// function updateSimilarVoteSummary(similarVoteSummary, mpSimilarVotes, mp) {
-//   if (!similarVoteSummary[mp.party]) {
-//     similarVoteSummary[mp.party] = {}
-//   }
-//
-//   mpSimilarVotes.forEach(similarVote => {
-//     const voteParty = mpIdToParty[similarVote.mpId]
-//     if (voteParty !== mp.party) {
-//       if (!similarVoteSummary[mp.party][voteParty]) {
-//         similarVoteSummary[mp.party][voteParty] = 0
-//       }
-//
-//       similarVoteSummary[mp.party][voteParty] += similarVote.similarVotes
-//     }
-//   })
-// }
-//
-// function getSortedSimilarVotes(similarVoteSummary, partyNames) {
-//   const sortedSimilarVotes = {}
-//
-//   partyNames.forEach(partyName => {
-//     const similarParties = Object.keys(similarVoteSummary[partyName])
-//
-//     sortedSimilarVotes[partyName] = similarParties.sort((a, b) => {
-//       const similarVotesA = similarVoteSummary[partyName][a]
-//       const similarVotesB = similarVoteSummary[partyName][b]
-//
-//       return similarVotesB - similarVotesA
-//     }).map(similarPartyName => {
-//       return {
-//         party: similarPartyName,
-//         similarVotes: similarVoteSummary[partyName][similarPartyName],
-//       }
-//     })
-//   })
-//
-//   return sortedSimilarVotes
-// }
-
 function updatePositionSummary(positionSummary, mpPositionSummary, mp) {
   if (!positionSummary[mp.party]) {
     positionSummary[mp.party] = {
@@ -148,25 +67,12 @@ function updatePositionSummary(positionSummary, mpPositionSummary, mp) {
 
 export default function process() {
   const voteSummary = {}
-  const nounSummary = {}
-  const similarVoteSummary = {}
-  const positionSummary = {}
-
   const partyNames = getPartyNames(mps)
 
   mps.forEach(mp => {
     updateVoteSummary(voteSummary, mpVoteSummaries[mp.id], mp)
-    updateNounSummary(nounSummary, mpNouns[mp.id], mp)
-    updateSimilarVoteSummary(similarVoteSummary, mpSimilarVotes[mp.id], mp)
   })
   calculateVotePercentages(partyNames, voteSummary)
-  const topPartyNouns = getTopNouns(nounSummary, partyNames)
-  const sortedSimilarVoteSummary = getSortedSimilarVotes(
-    similarVoteSummary,
-    partyNames
-  )
 
   writeToFile(voteSummary, 'data/export/party-vote-summary.json', true)
-  writeToFile(topPartyNouns, 'data/export/party-noun-lookup.json', true)
-  // writeToFile(sortedSimilarVoteSummary, 'data/export/party-similar-votes.json', true)
 }
