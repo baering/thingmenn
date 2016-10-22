1ee63e1425567fb6f73a19e17aa5696252eeee75
Bæring Gunnar Steinþórsson <baering90@gmail.com>
2016-10-22 03:42:01 +0000

    Backend: create top and bottom lists for attendance, stands taken and minutes talked

--- thingmenn-api/api/router.py
+++ thingmenn-api/api/router.py
@@ -1,6 +1,7 @@
 from api.mps import get_mps, get_mp_by_id, get_similar_mps, get_different_mps
 from api.parties import get_parties, get_party_by_id
 from api.summary import get_mp_vote_summary, get_party_vote_summary, get_mp_subject_summary, get_party_subject_summary, get_mp_nouns, get_party_nouns, get_mp_speech_statistics, get_party_speech_statistics
+from api.top import get_top_mp_attendance, get_bottom_mp_attendance, get_top_mp_minutes_talked, get_bottom_mp_minutes_talked, get_top_mp_stands_taken, get_bottom_mp_stands_taken
 
 from flask import blueprints
 from flask import jsonify
@@ -24,3 +25,11 @@ api.add_url_rule('/api/summary/nouns/mp/<string:mp_id>', 'get_mp_nouns', get_mp_
 api.add_url_rule('/api/summary/nouns/party/<string:party_id>', 'get_party_nouns', get_party_nouns)
 api.add_url_rule('/api/summary/speeches/mp/<string:mp_id>', 'get_mp_speech_statistics', get_mp_speech_statistics)
 api.add_url_rule('/api/summary/speeches/party/<string:party_id>', 'get_party_speech_statistics', get_party_speech_statistics)
+
+api.add_url_rule('/api/top/attendance/mps', 'get_top_mp_attendance', get_top_mp_attendance)
+api.add_url_rule('/api/top/minutes/mps', 'get_top_mp_minutes_talked', get_top_mp_minutes_talked)
+api.add_url_rule('/api/top/stands/mps', 'get_top_mp_stands_taken', get_top_mp_stands_taken)
+
+api.add_url_rule('/api/bottom/attendance/mps', 'get_bottom_mp_attendance', get_bottom_mp_attendance)
+api.add_url_rule('/api/bottom/minutes/mps', 'get_bottom_mp_minutes_talked', get_bottom_mp_minutes_talked)
+api.add_url_rule('/api/bottom/stands/mps', 'get_bottom_mp_stands_taken', get_bottom_mp_stands_taken)
---
+++ thingmenn-api/api/top/__init__.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+
+from flask import jsonify
+from flask import make_response
+from os import path
+import json
+from api.helpers import make_json_response, make_error
+
+mp_top_attendance = {}
+mp_top_minutes_talked = {}
+mp_top_stands_taken = {}
+
+with open(path.dirname(__file__) + '/../../data/mp-top-attendance.json', 'r') as f:
+    mp_top_attendance = json.loads(f.read())
+
+with open(path.dirname(__file__) + '/../../data/mp-top-minutes-talked.json', 'r') as f:
+    mp_top_minutes_talked = json.loads(f.read())
+
+with open(path.dirname(__file__) + '/../../data/mp-top-stands-taken.json', 'r') as f:
+    mp_top_stands_taken = json.loads(f.read())
+
+def get_top_mp_attendance():
+    return make_json_response(mp_top_attendance[0:10])
+
+def get_bottom_mp_attendance():
+    return make_json_response(mp_top_attendance[-10:])
+
+def get_top_mp_minutes_talked():
+    return make_json_response(mp_top_minutes_talked[0:10])
+
+def get_bottom_mp_minutes_talked():
+    return make_json_response(mp_top_minutes_talked[-10:])
+
+def get_top_mp_stands_taken():
+    return make_json_response(mp_top_stands_taken[0:10])
+
+def get_bottom_mp_stands_taken():
+    return make_json_response(mp_top_stands_taken[-10:])
