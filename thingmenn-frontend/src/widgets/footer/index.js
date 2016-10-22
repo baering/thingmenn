000f29f13da613b3de01a05da1e387397d11dc8c
Ægir Giraldo Þorsteinsson <aegir@thorsteinsson.is>
2016-10-22 01:30:06 +0000

    Fix footer text

--- thingmenn-frontend/src/widgets/footer/index.js
+++ thingmenn-frontend/src/widgets/footer/index.js
@@ -4,7 +4,7 @@ import './styles.css'
 const Footer = () => {
   return (
     <div className='Footer'>
-      Gögn birt með fyrirvara um villur í úrvinnslu. Gögnin eru frá þingi 143 (2013) til 145 (2016). Athugasemdir þegnar á <a className='u-link' href='mailto:thingmenn@aranja.com'>thingmenn@aranja.com</a>
+      Gögn birt með fyrirvara um villur í úrvinnslu. Gögnin eru frá þingi 143 (2013) til 145 (2016). Athugasemdir vel þegnar á <a className='u-link' href='mailto:thingmenn@aranja.com'>thingmenn@aranja.com</a>.
     </div>
   )
 }
