0ab09271a92a872aa3ad752df20dee70faa42371
David Bachmann <dabbi@dabbi.is>
2016-10-22 15:30:20 +0000

    Added meta facebook app_id, changed footer wording

--- thingmenn-frontend/src/widgets/footer/index.js
+++ thingmenn-frontend/src/widgets/footer/index.js
@@ -4,7 +4,7 @@ import './styles.css'
 const Footer = () => {
   return (
     <div className='Footer'>
-      Gögn birt með fyrirvara um villur í úrvinnslu. Gögnin eru frá þingi 143 <em>(2013)</em> til 145 <em>(2016)</em>. <br/>Athugasemdir vel þegnar á <a className='u-link' href='mailto:thingmenn@aranja.com'>thingmenn@aranja.com</a>.
+      Gögn birt með fyrirvara um villur í úrvinnslu. Gögnin eru fyrir kjörtímabilið 2013-2016. <br/>Athugasemdir vel þegnar á <a className='u-link' href='mailto:thingmenn@aranja.com'>thingmenn@aranja.com</a>.
     </div>
   )
 }
