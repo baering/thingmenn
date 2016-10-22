edb512c4d46118e4a6302b69cb8245d598f33a4b
David Bachmann <dabbi@dabbi.is>
2016-10-22 14:50:38 +0000

    Added meta tags. Added copy to about page.

--- thingmenn-frontend/src/components/about/index.js
+++ thingmenn-frontend/src/components/about/index.js
@@ -8,8 +8,10 @@ export default class About extends React.Component {
       <div className="fill">
         <h1 className="title">Um verkefnið</h1>
         <div className="About">
-          <p>Markmið þessa verkefnis er að sækja hluta af því mikla magni gagna sem finna má á vef <a className='u-link' href="http://althingi.is">Alþingis</a>, vinna úr þeim gögnum og setja fram á skemmtilegan og fræðandi máta.</p>
-          <p>Þetta verkefni er eftir <a className='u-link' href="http://twitter.com/baerinq" target="_blank">Bæring Gunnar Steinþórsson</a>, sérstakar þakkir fá <a className='u-link' href="http://twitter.com/justifycontent" target="_blank">Davíð Bachmann Jóhannesson</a>, <a className='u-link' href="http://twitter.com/herrhelms" target="_blank">Sebastian Helms</a> og <a className='u-link' href="http://twitter.com/thorsteinsson" target="_blank">Ægir Giraldo Þorsteinsson</a> fyrir hjálp við útfærslu og hönnun á vefnum.</p>
+          <p>Á þessum vef finnur þú upplýsingar og tölfræði um þingmenn og þingflokka fyrir kjörtímabilið 2013-2016.</p>
+          <p>Markmið þessa verkefnis er að sækja hluta af því mikla magni gagna sem finna má á vef <a className='u-link' href="http://althingi.is">Alþingis</a>, vinna úr þeim gögnum og setja fram á skemmtilegan, fræðandi og helst af öllu hlutlausan máta.</p>
+          <p>Þetta verkefni er eftir <a className='u-link' href="http://twitter.com/baerinq" target="_blank">Bæring Gunnar Steinþórsson</a>, sérstakar þakkir fá samstarfsfélagar mínir hjá <a className='u-link' href="http://aranja.com" target="_blank">Aranja</a>; <br/> <a className='u-link' href="http://twitter.com/justifycontent" target="_blank">Davíð Bachmann Jóhannesson</a>, <a className='u-link' href="http://twitter.com/herrhelms" target="_blank">Sebastian Helms</a> og <a className='u-link' href="http://twitter.com/thorsteinsson" target="_blank">Ægir Giraldo Þorsteinsson</a> fyrir hjálp við útfærslu og hönnun á vefnum.</p>
+          <p>Ábendingar og hugmyndir eru vel þegnar á <a className='u-link' href='mailto:thingmenn@aranja.com'>thingmenn@aranja.com</a>.</p>
         </div>
       </div>
     );
