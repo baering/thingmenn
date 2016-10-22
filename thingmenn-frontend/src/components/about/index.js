2a853b13c529b79652195efc9bc5146a56811ed7
David Bachmann <dabbi@dabbi.is>
2016-10-22 00:05:26 +0000

    Added link colors to about page.

--- thingmenn-frontend/src/components/about/index.js
+++ thingmenn-frontend/src/components/about/index.js
@@ -8,8 +8,8 @@ export default class About extends React.Component {
       <div className="fill">
         <h1 className="title">Um verkefnið</h1>
         <div className="About">
-          <p>Markmið þessa verkefnis er að sækja hluta af því mikla magni gagna sem finna má á vef <a href="http://althingi.is">Alþingis</a>, vinna úr þeim gögnum og setja fram á skemmtilegan og fræðandi máta.</p>
-          <p>Þetta verkefni er eftir <a href="http://twitter.com/baerinq" target="_blank">Bæring Gunnar Steinþórsson</a>, sérstakar þakkir fá <a href="http://twitter.com/justifycontent" target="_blank">Davíð Bachmann Jóhannesson</a>, <a href="http://twitter.com/herrhelms" target="_blank">Sebastian Helms</a> og <a href="http://twitter.com/thorsteinsson" target="_blank">Ægir Giraldo Þorsteinsson</a> fyrir hjálp við útfærslu og hönnun á vefnum.</p>
+          <p>Markmið þessa verkefnis er að sækja hluta af því mikla magni gagna sem finna má á vef <a className='u-link' href="http://althingi.is">Alþingis</a>, vinna úr þeim gögnum og setja fram á skemmtilegan og fræðandi máta.</p>
+          <p>Þetta verkefni er eftir <a className='u-link' href="http://twitter.com/baerinq" target="_blank">Bæring Gunnar Steinþórsson</a>, sérstakar þakkir fá <a className='u-link' href="http://twitter.com/justifycontent" target="_blank">Davíð Bachmann Jóhannesson</a>, <a className='u-link' href="http://twitter.com/herrhelms" target="_blank">Sebastian Helms</a> og <a className='u-link' href="http://twitter.com/thorsteinsson" target="_blank">Ægir Giraldo Þorsteinsson</a> fyrir hjálp við útfærslu og hönnun á vefnum.</p>
         </div>
       </div>
     );
