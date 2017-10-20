bdb1401361e06b52552ba47489b58a4f7829708b
Bæring Gunnar Steinþórsson <baering90@gmail.com>
2017-10-20 18:43:39 +0000

    Generate speech summaries for party by lthing and total

--- thingmenn-data/processor/speech-statistics/index.js
+++ thingmenn-data/processor/speech-statistics/index.js
@@ -1,14 +1,23 @@
 import { loadFile, writeToFile } from '../../utility/file'
+import {
+  getMpToPartyLookup,
+} from '../helpers'
 
 const mpSpeechAnalytics = loadFile('data/v2/mp-speech-statistics.json')
 
 export default function process() {
+  const mpToPartyId = getMpToPartyLookup()
+
   const mpSpeechSummaryByLthing = {}
   const mpSpeechSummaryTotal = {}
+
+  const partySpeechSummaryByLthing = {}
+  const partySpeechSummaryTotal = {}
 
   const lthings = Object.keys(mpSpeechAnalytics)
   lthings.forEach(lthing => {
     mpSpeechSummaryByLthing[lthing] = {}
+    partySpeechSummaryByLthing[lthing] = {}
 
     Object.keys(mpSpeechAnalytics[lthing]).forEach(mpId => {
       if (mpSpeechSummaryByLthing[lthing][mpId] === undefined) {
@@ -17,6 +26,16 @@ export default function process() {
 
       if (mpSpeechSummaryTotal[mpId] === undefined) {
         mpSpeechSummaryTotal[mpId] = {}
+      }
+
+      const mpPartyId = mpToPartyId[lthing][mpId]
+
+      if (partySpeechSummaryByLthing[lthing][mpPartyId] === undefined) {
+        partySpeechSummaryByLthing[lthing][mpPartyId] = {}
+      }
+
+      if (partySpeechSummaryTotal[mpPartyId] === undefined) {
+        partySpeechSummaryTotal[mpPartyId] = {}
       }
 
       const labels = Object.keys(mpSpeechAnalytics[lthing][mpId])
@@ -35,6 +54,20 @@ export default function process() {
           }
         }
 
+        if (partySpeechSummaryByLthing[lthing][mpPartyId][label] === undefined) {
+          partySpeechSummaryByLthing[lthing][mpPartyId][label] = {
+            count: 0,
+            minutes: 0,
+          }
+        }
+
+        if (partySpeechSummaryTotal[mpPartyId][label] === undefined) {
+          partySpeechSummaryTotal[mpPartyId][label] = {
+            count: 0,
+            minutes: 0,
+          }
+        }
+
         const countForLabel = mpSpeechAnalytics[lthing][mpId][label].count
         const minutesForLabel = mpSpeechAnalytics[lthing][mpId][label].minutes
 
@@ -44,6 +77,12 @@ export default function process() {
 
           mpSpeechSummaryTotal[mpId][label].count += countForLabel
           mpSpeechSummaryTotal[mpId][label].minutes += minutesForLabel
+
+          partySpeechSummaryByLthing[lthing][mpPartyId][label].count += countForLabel
+          partySpeechSummaryByLthing[lthing][mpPartyId][label].minutes += minutesForLabel
+
+          partySpeechSummaryTotal[mpPartyId][label].count += countForLabel
+          partySpeechSummaryTotal[mpPartyId][label].minutes += minutesForLabel
         }
       })
     })
@@ -51,4 +90,7 @@ export default function process() {
 
   writeToFile(mpSpeechSummaryByLthing, 'data/export-v2/by-lthing/mp-speech-summaries.json', true)
   writeToFile(mpSpeechSummaryTotal, 'data/export-v2/total/mp-speech-summaries.json', true)
+
+  writeToFile(partySpeechSummaryByLthing, 'data/export-v2/by-lthing/party-speech-summaries.json', true)
+  writeToFile(partySpeechSummaryTotal, 'data/export-v2/total/party-speech-summaries.json', true)
 }
