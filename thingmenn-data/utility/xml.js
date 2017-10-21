01cd8092c3a6e5d0bf3448640361603aace1de06
Bæring Gunnar Steinþórsson <baering90@gmail.com>
2017-10-21 17:22:20 +0000

    Data: Allow fetchXml a few tries if it fails

--- thingmenn-data/utility/xml.js
+++ thingmenn-data/utility/xml.js
@@ -21,11 +21,23 @@ export async function fetchXml(url) {
     console.log(`fetchXml (cached):\t${url}`)
     return cache[url]
   }
-  const xml = await axios.get(url)
+
+  let tries = 0
+  let xml = undefined
+
+  while (xml === undefined) {
+    try {
+      console.log(`fetchXml (request)\t${url}`)
+      tries += 0
+      if (tries >= 4) {
+        break
+      }
+      xml = await axios.get(url)
+    } catch (e) {
+      console.log('Failed. Retrying (', tries, ')')
+    }
+  }
   const result = await parseXmlPromiseWrapper(xml.data)
-
   cache[url] = result
-
-  console.log(`fetchXml (request)\t${url}`)
   return result
 }
