7ba50c640f0ab34cbe35bef9f629f38b7e425c8a
David Bachmann <dabbi@dabbi.is>
2017-10-22 19:01:24 +0000

    Style document view

--- thingmenn-frontend/src/widgets/documents/index.js
+++ thingmenn-frontend/src/widgets/documents/index.js
@@ -9,57 +9,55 @@ const Documents = ({ documentSummary }) => {
   if (!(documentSummary && documentSummary.summary)) {
     return null
   }
-  return (
-    <div className="Documents">
-      <h2>1. flutningsmaður</h2>
+  return <div className="Documents">
       <div className="Documents-items">
+        <h2 className="Documents-subheader">1. flutningsmaður</h2>
         <div className="Documents-item" key={document.id}>
-          <p className="Documents-statsText">{documentSummary.summary.bills.presenter.count}</p>
-          <h1 className="Documents-statsHeading">
-            Frumvörp
-          </h1>
+          <p className="Documents-statsText">
+            {documentSummary.summary.bills.presenter.count}
+          </p>
+          <h1 className="Documents-statsHeading">Frumvörp</h1>
         </div>
         <div className="Documents-item" key={document.id}>
-          <p className="Documents-statsText">{documentSummary.summary.motions.presenter.count}</p>
-          <h1 className="Documents-statsHeading">
-            Þingsályktunartillögur
-          </h1>
+          <p className="Documents-statsText">
+            {documentSummary.summary.motions.presenter.count}
+          </p>
+          <h1 className="Documents-statsHeading">Þingsályktunartillögur</h1>
         </div>
       </div>
 
-      <h2>Meðflutningsmaður</h2>
       <div className="Documents-items">
+      <h2 className="Documents-subheader">Meðflutningsmaður</h2>
         <div className="Documents-item" key={document.id}>
-          <p className="Documents-statsText">{documentSummary.summary.motions.coPresenter.count}</p>
-          <h1 className="Documents-statsHeading">
-            Frumvörp
-          </h1>
+          <p className="Documents-statsText">
+            {documentSummary.summary.motions.coPresenter.count}
+          </p>
+          <h1 className="Documents-statsHeading">Frumvörp</h1>
         </div>
         <div className="Documents-item" key={document.id}>
-          <p className="Documents-statsText">{documentSummary.summary.motions.coPresenter.count}</p>
-          <h1 className="Documents-statsHeading">
-            Þingsályktunartillögur
-          </h1>
+          <p className="Documents-statsText">
+            {documentSummary.summary.motions.coPresenter.count}
+          </p>
+          <h1 className="Documents-statsHeading">Þingsályktunartillögur</h1>
         </div>
       </div>
 
-      <h2>Fyrirspurnir</h2>
       <div className="Documents-items">
+      <h2 className="Documents-subheader">Fyrirspurnir</h2>
         <div className="Documents-item" key={document.id}>
-          <p className="Documents-statsText">{documentSummary.summary.inquiries.asked}</p>
-          <h1 className="Documents-statsHeading">
-            Spurningar
-          </h1>
+          <p className="Documents-statsText">
+            {documentSummary.summary.inquiries.asked}
+          </p>
+          <h1 className="Documents-statsHeading">Spurningar</h1>
         </div>
         <div className="Documents-item" key={document.id}>
-          <p className="Documents-statsText">{documentSummary.summary.inquiries.answered}</p>
-          <h1 className="Documents-statsHeading">
-            Svör
-          </h1>
+          <p className="Documents-statsText">
+            {documentSummary.summary.inquiries.answered}
+          </p>
+          <h1 className="Documents-statsHeading">Svör</h1>
         </div>
       </div>
     </div>
-  )
 }
 
 Documents.propTypes = {
