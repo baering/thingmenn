588f56efa573f3afb5cda7ba2568ac3fdbe3d70f
Bæring Gunnar Steinþórsson <baering@aranja.com>
2024-11-29 10:29:01 +0000

    Fix color issue on chrome for android (#36)

--- thingmenn-frontend/src/widgets/color-legend/index.js
+++ thingmenn-frontend/src/widgets/color-legend/index.js
@@ -5,18 +5,22 @@ const ColorLegend = ({ includeAbsent }) => {
   return (
     <div className="Colors">
       <div className="Colors--standsTaken">
-        ◼︎ <span className="Colors--grey">Afstaða tekin</span>
+        <span className="ColorBox ColorBox--standsTaken" />{' '}
+        <span className="Colors--grey">Afstaða tekin</span>
       </div>
       <div className="Colors--idle">
-        ◼︎ <span className="Colors--grey">Afstöðuleysi</span>
+        <span className="ColorBox ColorBox--idle" />{' '}
+        <span className="Colors--grey">Afstöðuleysi</span>
       </div>
       {includeAbsent ? (
         <div className="Colors--absent">
-          ◼︎ <span className="Colors--grey">Tilkynnt fjarvist</span>
+          <span className="ColorBox ColorBox--absent" />{' '}
+          <span className="Colors--grey">Tilkynnt fjarvist</span>
         </div>
       ) : null}
       <div className="Colors--away">
-        ◼︎ <span className="Colors--grey">Fjarverandi</span>
+        <span className="ColorBox ColorBox--away" />{' '}
+        <span className="Colors--grey">Fjarverandi</span>
       </div>
     </div>
   )
